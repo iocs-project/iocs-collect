39698f02eea04c16931fcfe9108973d2bf0c454b
Jan Cakl <cakl.jan@gmail.com>
2025-05-15 12:48:00 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 set -e
 
-# 1. Klonuj závislý projekt, pokud ještě není
 if [ ! -d "db-postgre-sql" ]; then
   git clone https://github.com/SecurityChecker/db-postgre-sql.git
 fi
 
-# 2. Spusť docker compose z něj
 cd db-postgre-sql
 docker-compose up -d --build
-python3 ./main.py
-cd ..
 
 if [ ! -d "venv" ]; then
   python3 -m venv venv
 fi
 
-# 3. Aktivuj venv
 source venv/bin/activate
 
-# 4. Nainstaluj dependencies
 pip install --upgrade pip
+pip install -r requirements.txt
+
+python3 ./main.py
+cd ..
+
 pip install -r requirements.txt
 
 python3 ./main.py
